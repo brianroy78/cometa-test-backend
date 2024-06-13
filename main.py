0f88768be5181a4a2fde0b9d0159888cd61885f3
Brian Roy Sagredo Lijeron <brianroy78@hotmail.com>
2024-06-12 22:14:27 -0400

    deleting changes

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from endpoints import endpoints
-from scripts.data_intialization import initialize_data
-
-app = FastAPI()
-app.include_router(endpoints.router)
-
-initialize_data()
